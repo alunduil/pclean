ea6a80839e3bc606fdb47760b6abaabd71b7ee6b
Alex Brandt <alunduil@alunduil.com>
2013-10-12 23:43:28 -0500

    Add metainformation to pclean.
    Created a special purpose module in pclean for the metainformation of
    the project called surprisingly enough, ``pclean.information``.

---
+++ pclean/information.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2013 by Alex Brandt <alunduil@alunduil.com>
+#
+# margarine is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+NAME = 'pclean'
+VERSION = '9999'
+DESCRIPTION = \
+        'Linting utility for portage configuration files.'
+LONG_DESCRIPTION = \
+        'Utility to lint all of the configuration files for portage (i.e. ' \
+        '/etc/portage/package.use &c).'
+AUTHOR = 'Alex Brandt'
+AUTHOR_EMAIL = 'alunduil@alunduil.com'
+URL = 'https://github.com/alunduil/pclean'
+LICENSE = 'MIT'
+
+COPY_YEAR = '2013'
